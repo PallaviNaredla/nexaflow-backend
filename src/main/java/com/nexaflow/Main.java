1dc78745df8f45f24dcd9a328db2d256800da547
PallaviNaredla <pallavinaredla2505@gmail.com>
2026-09-18 11:23:30 +0530

    Day 3: Implemented user data handling and product total calculation

--- src/main/java/com/nexaflow/Main.java
+++ src/main/java/com/nexaflow/Main.java
@@ -2,7 +2,25 @@ package com.nexaflow;
 
 public class Main {
     public static void main(String[] args) {
+        String name="Pallavi Naredla";
+        String email="pallavinaredla@gmail.com";
+        int age=21;
+        System.out.println("User Details");
+        System.out.println("Name :"+name);
+        System.out.println("Email :"+email);
+        System.out.println("Age :"+age);
 
-        System.out.println("NexaFlow Backend Initialized");
+        String productName = "Laptop";
+        double price = 75000.0;
+        int quantity = 2;
+
+        double total = price * quantity;
+
+        System.out.println("\nProduct Details:");
+        System.out.println("Product: " + productName);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total Price: " + total);
+
     }
 }
